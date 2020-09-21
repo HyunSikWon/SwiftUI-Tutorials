c0d5c72328a8331ddc5c44750867eb155c1400bf
hyunsik won <shik0601@gmail.com>
2020-09-21 21:00:04 +0900

    SwiftUI First Commit

---
+++ Chapter 1/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 원현식 on 2020/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
