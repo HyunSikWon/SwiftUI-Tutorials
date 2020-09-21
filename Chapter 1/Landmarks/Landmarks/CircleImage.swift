dccbd41f535a9fc088ca1762cfbe32e4e8cfd41c
hyunsik won <shik0601@gmail.com>
2020-09-21 22:23:22 +0900

    2020.09.21 SwiftUI Tutorial

---
+++ Chapter 1/Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 원현식 on 2020/09/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
